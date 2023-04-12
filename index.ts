2735e499dd7cfed5bb275080897f1bfc0fe85d49
Jiarr <114872804+jiarrdgr8@users.noreply.github.com>
2023-04-12 17:11:25 +0800

    saving

--- index.ts
+++ index.ts
@@ -121,4 +121,4 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
-app.listen(5001, ()=> console.log('Server started on port 5000'))
+app.listen(5000, ()=> console.log('Server started on port 5000'))
